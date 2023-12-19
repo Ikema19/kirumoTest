284f1285e4eaf37389b29b0373c67a4446852b23
Ikema19 <masa0208yuki@icloud.com>
2023-12-19 22:15:42 +0900

    1.ejsの遷移設定

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const cors = require('cors');
 app.use(cors());
 const PORT = process.env.PORT || 3001;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  // 他の必要なCORSヘッダーも追加できます
+  // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 //BODYPARSER
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
